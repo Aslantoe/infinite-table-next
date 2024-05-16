3094efbed3f7f5faa53331b8f3117d0352d03204
卜祥昊 <buxh@qdcares.cn>
2024-05-16 10:49:44 +0800

    ✨ feat: 删除tsx组件文件

--- src/hooks/utils.ts
+++
@@ -1,5 +0,0 @@
-import TableColumnItem from '@/hooks/useTableColumnItemHooks';
-
-export function isSameColumn(column1: TableColumnItem, column2: TableColumnItem) {
-  return column1 === column2;
-}
--- src/render/headerRenders.tsx
+++ src/render/headerRenders.tsx
@@ -1,58 +1,47 @@
-import { h, VNode, inject } from "vue";
-import { ColumnRenderProps, HeaderRenderProps, tableOptionsInjectKey } from "@/common/types";
-import useTableStore from "@/table-store";
+import { ColumnRenderProps, HeaderRenderProps } from "@/common/types";
 
-export function defaultHeaderRender(
-  _h: typeof h,
-  { options }: HeaderRenderProps,
-): VNode {
+export function defaultHeaderRender(h, { options }: HeaderRenderProps) {
   return <span>{options.label}</span>;
 }
-/**
- * header checkbox
- */
-export function checkboxHeaderRender(_h: typeof h, { tableStore }: HeaderRenderProps): VNode {
-  const tableOptions: any = inject(tableOptionsInjectKey);
-  const { addSelectedRows, clearSelectedRows, tableData } = useTableStore(tableOptions, 'id', 'header-render')
-  return <input
-    class="infinite-table__cell--checkbox"
-    type="checkbox"
-    onInput={(e: MouseEvent) => {
-      const checkbox = (e.currentTarget) as HTMLInputElement;
-      if (checkbox.checked) {
-        addSelectedRows(tableData.value.slice());
-      } else {
-        clearSelectedRows();
-      }
-    }}
-  />;
+
+export function checkboxHeaderRender(h, { tableStore }: HeaderRenderProps) {
+  return (
+    <input
+      class="infinite-table__cell--checkbox"
+      type="checkbox"
+      onInput={(e: MouseEvent) => {
+        const checkbox = e.currentTarget as HTMLInputElement;
+        if (checkbox.checked) {
+          tableStore.addSelectedRows(...tableStore.tableData.slice());
+        } else {
+          tableStore.clearSelectedRows();
+        }
+      }}
+    />
+  );
 }
 
-/**
- * column checkbox
- */
 export const checkboxColumnRender = function checkboxColumnRender(
-  _h: typeof h,
-  { row, tableStore }: ColumnRenderProps,
+  h,
+  { row, tableStore }: ColumnRenderProps
 ) {
-  const tableOptions: any = inject(tableOptionsInjectKey);
-  const { addSelectedRows, removeSelectedRows, isRowSelected } = useTableStore(tableOptions, 'id', 'header-render')
   return (
-    <div class="infinite-table__cell__content--selection"
-         onClick={(evt: MouseEvent) => {
-           evt.stopPropagation();
-           const checked = isRowSelected(row);
-           if (!checked) {
-             addSelectedRows(row);
-           } else {
-             removeSelectedRows(row);
-           }
-         }}
+    <div
+      class="infinite-table__cell__content--selection"
+      onClick={(evt: MouseEvent) => {
+        evt.stopPropagation();
+        const checked = tableStore.isRowSelected(row);
+        if (!checked) {
+          tableStore.addSelectedRows(row);
+        } else {
+          tableStore.removeSelectedRows(row);
+        }
+      }}
     >
       <input
         class="infinite-table__cell--checkbox"
-        type='checkbox'
-        checked={isRowSelected(row)}
+        type="checkbox"
+        checked={tableStore.isRowSelected(row)}
       />
     </div>
   );
